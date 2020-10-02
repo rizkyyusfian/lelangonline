641d41b6e9a926bafe1ecad1edd97c856421db77
Muhammad Rizky Yusfian Yusuf <yusfian.zoldyck@gmail.com>
2020-10-02 15:06:14 +0900

    buat index,php
    buat index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+</head>
+<body>
+	<h1>Auksien Onlen</h1>
+    <p>your best bid make the item valuable</p>
+	<a href="login.php">Login</a>
+	<br><br>
+	<a href="register.php">Register</a>
+</body>
+</html>
