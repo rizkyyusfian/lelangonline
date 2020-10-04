4288e24a4a5a28e442980630cb96ffb6a7238bba
Muhammad Rizky Yusfian Yusuf <yusfian.zoldyck@gmail.com>
2020-10-04 19:33:34 +0900

    Revert "Revert "Merge pull request #4 from rizkyyusfian/revert-2-desain""
    This reverts commit 97ff215dd278effe868faef2b2725a28b95ca875.

--- login.php
+++ login.php
@@ -7,41 +7,10 @@
 	}
 ?>
 
-<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
-<!--[if !IE]><!-->
 <!DOCTYPE html>
-<html lang="en" class="no-js">
-<!--<![endif]-->
-<!-- BEGIN HEAD -->
+<html>
 <head>
-	<meta charset="utf-8"/>
 	<title>Login</title>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-<meta content="" name="description"/>
-<meta content="" name="author"/>
-<meta name="MobileOptimized" content="320">
-<!-- BEGIN GLOBAL MANDATORY STYLES -->
-<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
-<!-- END GLOBAL MANDATORY STYLES -->
-<!-- BEGIN PAGE LEVEL STYLES -->
-<link rel="stylesheet" type="text/css" href="assets/plugins/select2/select2.css"/>
-<!-- END PAGE LEVEL SCRIPTS -->
-<!-- BEGIN THEME STYLES -->
-<link href="assets/css/style-conquer.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/style-responsive.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/plugins.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/themes/default.css" rel="stylesheet" type="text/css" id="style_color"/>
-<link href="assets/css/pages/login.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
-<!-- END THEME STYLES -->
-<link rel="shortcut icon" href="favicon.ico"/>
 </head>
 <body>
 	<?php
@@ -51,93 +20,44 @@
 			unset($_SESSION['error']);
 		}
 	?>
-	<!-- BEGIN BODY -->
-<body class="login">
-<!-- BEGIN LOGO -->
-<div class="logo">
-	<a href="">
-	<img src="" alt=""/>
-	</a>
-</div>
-<!-- END LOGO -->
-<!-- BEGIN LOGIN -->
-<div class="content">
-	<!-- BEGIN LOGIN FORM -->
-	<form class="login-form" action="process/login_process.php" method="post">
-		<h3 class="form-title">Login to your account</h3>
-		<div class="alert alert-danger display-hide">
-			<button class="close" data-close="alert"></button>
-			<span>
-			Enter any username and password. </span>
-		</div>
-		<div class="form-group">
-			<!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-			<label class="control-label visible-ie8 visible-ie9">Username</label>
-			<div class="input-icon">
-				<i class="fa fa-user"></i>
-				<input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="idlogin" required/>
-			</div>
-		</div>
-		<div class="form-group">
-			<label class="control-label visible-ie8 visible-ie9">Password</label>
-			<div class="input-icon">
-				<i class="fa fa-lock"></i>
-				<input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="passlogin" required/>
-			</div>
-		</div>
-		<div class="form-actions">
-			<button type="submit" class="btn btn-info pull-right" name="btnsubmit">
-			Login </button>
-		</div>
-		<div class="create-account">
-			<p>
-				 Don't have an account yet ?&nbsp; <a href="register.php" id="register-btn">Create an account</a>
-			</p>
-		</div>
+
+	<form method="POST" action="process/login_process.php">
+		<h2>Please Login to use the website</h2>
+		<table>
+			<tr>
+				<td><label>Username :</label></td>
+				<td><input type="text" name="idlogin" placeholder="Input Id User"></td>
+			</tr>
+			<tr>
+				<td><label>Password :</label></td>
+				<td><input type="password" name="passlogin" placeholder="Input Password"></td>
+			</tr>
+		</table>
+		<br>
+		<input type="submit" name="btnsubmit" value="Login">
+		<p>Belum Punya Akun?<a href="register.php">Daftar Disini!</a></p>
+
+		<p>Daftar Akun :</p>
+		<ul>
+			<li>username: yusfian</li>
+			<li>password: yusfian</li>
+		</ul>
+		<ul>
+			<li>username: arison</li>
+			<li>password: arison</li>
+		</ul>
+		<ul>
+			<li>username: admin</li>
+			<li>password: admin</li>			
+		</ul>
+		<ul>
+			<li>username: jojo</li>
+			<li>password: jojo1</li>	
+		</ul>
+		<ul>
+			<li>username: dio</li>
+			<li>password: 123</li>
+		</ul>	
 	</form>
-	<!-- END LOGIN FORM -->	
-<!-- END LOGIN -->
-<!-- BEGIN COPYRIGHT -->
-<div class="copyright">
-	2020 &copy;MRYY. Admin Dashboard Template.
-</div>
-<!-- END COPYRIGHT -->
-<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-<!-- BEGIN CORE PLUGINS -->
-<script src="assets/plugins/jquery-1.11.0.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
-<!-- IMPORTANT! Load jquery-ui-1.10.3.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
-<script src="assets/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
-<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="assets/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="assets/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
-<!-- END CORE PLUGINS -->
-<!-- BEGIN PAGE LEVEL PLUGINS -->
-<script src="assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="assets/plugins/select2/select2.min.js"></script>
-<!-- END PAGE LEVEL PLUGINS -->
-<!-- BEGIN PAGE LEVEL SCRIPTS -->
-<script src="assets/scripts/app.js" type="text/javascript"></script>
-<script src="assets/scripts/login.js" type="text/javascript"></script>
-<!-- END PAGE LEVEL SCRIPTS -->
-<script>
-jQuery(document).ready(function() {     
-  App.init();
-  Login.init();
-  var action = location.hash.substr(1);
-          if (action == 'createaccount') {
-              $('.register-form').show();
-              $('.login-form').hide();
-              $('.forget-form').hide();
-          } else if (action == 'forgetpassword')  {
-              $('.register-form').hide();
-              $('.login-form').hide();
-              $('.forget-form').show();
-          }
-});
-</script>
-<!-- END JAVASCRIPTS -->
 </body>
 </html>
