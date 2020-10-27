ef02000178e23183e7d2eb852a11275e36ee1817
rizkyyusfian <yusfian.zoldyck@gmail.com>
2020-10-27 17:32:59 +0900

    Update Index.php + Minor Fix Login & Register
    mengubah tampilan desain file Index.php dan memperbaiki tampilan di login dan register

--- login.php
+++ login.php
@@ -95,7 +95,7 @@
 <!-- END LOGIN -->
 <!-- BEGIN COPYRIGHT -->
 <div class="copyright">
-	2020 &copy;MRYY. Admin Dashboard Template.
+	2020 &copy;AuksienTeam
 </div>
 <!-- END COPYRIGHT -->
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
--- register.php
+++ register.php
@@ -97,7 +97,7 @@
 <!-- END REGISTER -->
 <!-- BEGIN COPYRIGHT -->
 <div class="copyright">
-	 2020 &copy;MRYY. Admin Dashboard Template.
+	 2020 &copy;AuksienTeam
 </div>
 <!-- END COPYRIGHT -->
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
