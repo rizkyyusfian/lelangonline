3b2e1bde7efb7a2e20b0338c4217bd03d904926b
rizkyyusfian <yusfian.zoldyck@gmail.com>
2020-10-27 17:59:28 +0900

    minor changes

--- login.php
+++ login.php
@@ -37,7 +37,7 @@
 <link href="assets/css/pages/login.css" rel="stylesheet" type="text/css"/>
 <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
 <!-- END THEME STYLES -->
-<link rel="shortcut icon" href="favicon.ico"/>
+<link rel="shortcut icon" href=""/>
 </head>
 <body>
 	<?php
--- register.php
+++ register.php
@@ -28,7 +28,7 @@
 <link href="assets/css/pages/login.css" rel="stylesheet" type="text/css"/>
 <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
 <!-- END THEME STYLES -->
-<link rel="shortcut icon" href="favicon.ico"/>
+<link rel="shortcut icon" href=""/>
 </head>
 <!-- BEGIN BODY -->
 <body class="login">
